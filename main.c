fe38bb4f6f4fd1f98f10b32a73eeab18669e8902
Jyotsna Kukreti <jyots394@gmail.com>
2020-08-19 00:42:35 +0530

    after rebasing from master

--- main.c
+++ main.c
@@ -5,9 +5,15 @@ Initial Function()
   //Sample Code
 }
 
+<<<<<<< f71ccdccb44d9637abf24e4a39f5976da5baf587
 Initial Security()
 {
  //Sample Code
+=======
+feature1()
+{
+  //Sample Code for Feature 1
+>>>>>>> added feature A
 }
 
 
